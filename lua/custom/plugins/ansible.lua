8f352a2270125f580a86fd3c6ecc325597421d96
alex <aalexx.albrecht@gmail.com>
2025-03-02 13:39:40 +0100

    ansible lsp and plugin

---
+++ lua/custom/plugins/ansible.lua
@@ -0,0 +1,9 @@
+return {
+  'mfussenegger/nvim-ansible',
+  config = function()
+    vim.keymap.set('v', '<leader>te', function()
+      require('ansible').run()
+    end, { buffer = true, desc = 'Ansible: run selected task' })
+    vim.keymap.set('n', '<leader>te', ":w<CR> :lua require('ansible').run()<CR>", { buffer = true, desc = 'Ansible: run current playbook' })
+  end,
+}
