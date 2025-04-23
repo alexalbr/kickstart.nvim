c25b44b663c7a9f9337a12fe05a91bc0d467a82d
alex <aalexx.albrecht@gmail.com>
2025-04-23 20:24:15 +0200

    terraformls

--- init.lua
+++ init.lua
@@ -669,6 +669,7 @@ require('lazy').setup({
         yamlls = {},
         bashls = {},
         helm_ls = {},
+        terraformls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
